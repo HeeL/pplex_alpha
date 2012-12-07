e744c232864f383c5c724cbfba22faf44120fd2c
HeeL <parizhskiy@gmail.com>
2012-12-07 07:24:47 +0100

    refactored and added new lang specs

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -2,14 +2,31 @@ require 'spec_helper'
 
 describe Language do
   subject { Language }
+  
+  context 'match found' do
+    before :each do
+      @lang1 = FactoryGirl.create(:language, name: 'Ruby')
+      @lang2 = FactoryGirl.create(:language, name: 'Ruby++')
+    end
 
-  it "looks for matching names" do
-    lang1 = FactoryGirl.create(:language, name: 'Ruby')
-    lang2 = FactoryGirl.create(:language, name: 'Ruby++')
-    subject.match_names('Ru').should eql([lang1, lang2])
-    subject.match_names('Ruby').should eql([lang1, lang2])
-    subject.match_names('Ruby+').should eql([lang2])
-    subject.match_names('Ruby+++').empty?.should be_true
+    it "finds matching names" do
+      subject.match_names('Ru').should eql([@lang1, @lang2])
+      subject.match_names('Ruby').should eql([@lang1, @lang2])
+      subject.match_names('Ruby+').first.should eql(@lang2)
+    end
+
+    it 'ignores the case of letters' do
+      subject.match_names('RuBy++').first.should eql(@lang2)
+    end
+  end
+  
+  context 'nothing found' do
+    let(:result) { subject.match_names('something') }
+
+    it 'returns an empty array' do
+      result.empty?.should be_true
+      result.should be_kind_of(Array)
+    end
   end
 
 end
