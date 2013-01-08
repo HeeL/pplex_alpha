704a5b4964bd5f66129bfbc6faba311dcae02765
HeeL <parizhskiy@gmail.com>
2013-01-08 23:02:24 +0100

    deleted migration that relies on paperclip gem

--- db/migrate/20130102211052_remove_language_logo.rb
+++
@@ -1,8 +0,0 @@
-class RemoveLanguageLogo < ActiveRecord::Migration
-  def up
-    remove_column :languages, :logo_file_name
-    remove_column :languages, :logo_content_type
-    remove_column :languages, :logo_file_size
-    remove_column :languages, :logo_updated_at
-  end
-end
