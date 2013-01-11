f0feeab248495276dc71b59753f1b02d6e72c7f6
HeeL <parizhskiy@gmail.com>
2013-01-11 23:28:57 +0100

    error messages from server side

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-  def js_msg(text_arr)
-    js = text_arr.map{|text| "show_message(\"#{text.gsub(/"/, "'")}\");" if text.present?}.reject{|i| i.nil?}
+  def js_msg(text_arr, type = '')
+    js = text_arr.map{|text| "show_message(\"#{text.gsub(/"/, "'")}\"#{', "", "error"' if type == 'error'});" if text.present?}.reject{|i| i.nil?}
     javascript_tag("$(document).ready(function(){ #{js.join} });") unless js.empty?
   end
 
