931c31945a7d0b9bdac13f88d053cddc19c87ef9
HeeL <parizhskiy@gmail.com>
2013-01-20 11:10:39 +0100

    added user id param to robokassa

--- app/controllers/balance_controller.rb
+++ app/controllers/balance_controller.rb
@@ -9,21 +9,22 @@ class BalanceController < ApplicationController
   def process_payment
     amount = params[:OutSum]
     status = 'fail'
-    signature = md5("#{amount}:#{params[:InvId]}:#{ENV['ROBO_PASS2']}").upcase
+    signature = md5("#{amount}:#{params[:InvId]}:#{ENV['ROBO_PASS2']}:shp_user=#{params[:shp_user]}").upcase
     if signature == params[:SignatureValue]
       status = 'success'
-      current_user.add_money(amount)
+      User.find(params[:shp_user]).add_money(amount)
     end
     render text: status
   end
 
   def robokassa
     amount = params[:amount].to_i
-    signature =  md5("#{ENV['ROBO_LOGIN']}:#{amount}:0:#{ENV['ROBO_PASS1']}")
+    signature =  md5("#{ENV['ROBO_LOGIN']}:#{amount}:0:#{ENV['ROBO_PASS1']}:shp_user=#{current_user.id}")
     request = {
       outSum: amount,
       signatureValue: signature,
       incCurrLabel: ENV['ROBO_PAY_METHOD'],
+      shp_user: current_user.id,
       mrchLogin: ENV['ROBO_LOGIN'],
       desc: 'Increase balance on pplex.net',
       culture: 'en',
